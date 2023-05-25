b6d1990d005675b8ab89eecd6b9c44c4186926a5
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-05-25 13:13:19 +0800

    Added cors support

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-var cors = require('cors')
+var cors = require("cors");
 
 const app = express();
 const port = 4000;
 
-app.use(cors())
+app.use(cors());
 app.get("/", (req, res) => {
   res.json({
     appFlavor: process.env.APP_FLAVOR || "black",
