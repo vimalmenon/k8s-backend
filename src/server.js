26c0d4014894c095df49c8878cdbdc3734459017
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-05-26 07:07:56 +0800

    Add Other attributes to the API response

--- src/server.js
+++ src/server.js
@@ -6,8 +6,13 @@ const port = 4000;
 
 app.use(cors());
 app.get("/", (req, res) => {
+  const d = new Date();
   res.json({
     appFlavor: process.env.APP_FLAVOR || "black",
+    nodeName: process.env.NODE_NAME,
+    podName: process.env.POD_NAME,
+    podIp: process.env.POD_IP,
+    date: d.toLocaleString()
   });
 });
 
